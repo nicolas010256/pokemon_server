c612a2d6d25c4b74fc610a13c11b73a45e914da2
nicolas <nicolas010256@gmail.com>
2019-05-01 16:47:48 -0300

    ADD: /pokemon/:id; ENABLED: CrossOrigin

--- src/main/java/pokemon/server/controller/PokemonController.java
+++ src/main/java/pokemon/server/controller/PokemonController.java
@@ -1,9 +1,7 @@
 package pokemon.server.controller;
 
-import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
-import java.util.List;
 
 import org.json.JSONArray;
 import org.json.JSONObject;
@@ -11,14 +9,16 @@ import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class PokemonController {
+    @CrossOrigin
     @GetMapping("/pokemon")
-    public ResponseEntity getPokemon() {
+    public ResponseEntity<String> getPokemon() {
         JSONObject json = new JSONObject();
         try {
             StringBuffer buffer = new StringBuffer();
@@ -26,6 +26,7 @@ public class PokemonController {
             while (reader.ready()) {
                 buffer.append((char) reader.read());
             }
+            reader.close();
             JSONArray data = new JSONArray(buffer.toString());
             json.put("code", 200);
             json.put("message", "Success");
@@ -42,5 +43,38 @@ public class PokemonController {
 
             return new ResponseEntity<String>(json.toString(), HttpStatus.NOT_FOUND);
         }
+    }
+
+    @CrossOrigin
+    @GetMapping("/pokemon/{id}")
+    public ResponseEntity<String> getPokemon(@PathVariable("id") long id) {
+        JSONObject json = new JSONObject();
+        HttpHeaders header = new HttpHeaders();
+        header.setContentType(MediaType.APPLICATION_JSON);
+        
+        try {
+            String data = readFile(getClass().getResource("/pokemon/" + id + ".json").getFile());
+            json.put("code", 200);
+            json.put("message", "Success");
+            json.put("data", new JSONObject(data));
+
+            return new ResponseEntity<String>(json.toString(), header, HttpStatus.OK);
+        } catch (IOException e) {
+            json.put("code", 404);
+            json.put("message", "Resource Not Found");
+            json.put("data", JSONObject.NULL);
+
+            return new ResponseEntity<String>(json.toString(), header, HttpStatus.NOT_FOUND);
+        }
+    }
+
+    private String readFile(String file) throws IOException {
+        StringBuffer buffer = new StringBuffer();
+        FileReader reader = new FileReader(file);
+        while (reader.ready()) {
+            buffer.append((char) reader.read());
+        }
+        reader.close();
+        return buffer.toString();
     }
 }
