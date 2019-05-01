5d0ac1406979afe9153bcce777b167723f78dd31
nicolas <nicolas010256@gmail.com>
2019-05-01 18:29:11 -0300

    FIX: PokemonController

--- src/main/java/pokemon/server/controller/PokemonController.java
+++ src/main/java/pokemon/server/controller/PokemonController.java
@@ -19,29 +19,16 @@ public class PokemonController {
     @CrossOrigin
     @GetMapping("/pokemon")
     public ResponseEntity<String> getPokemon() {
-        JSONObject json = new JSONObject();
+        JSONArray json = null;
+        HttpHeaders header = new HttpHeaders();
+        header.setContentType(MediaType.APPLICATION_JSON);
+        
         try {
-            StringBuffer buffer = new StringBuffer();
-            FileReader reader = new FileReader(getClass().getResource("/pokemon/all.json").getFile());
-            while (reader.ready()) {
-                buffer.append((char) reader.read());
-            }
-            reader.close();
-            JSONArray data = new JSONArray(buffer.toString());
-            json.put("code", 200);
-            json.put("message", "Success");
-            json.put("data", data);
-
-            HttpHeaders header = new HttpHeaders();
-            header.setContentType(MediaType.APPLICATION_JSON);
-
+            String data = readFile(getClass().getResource("/pokemon/all.json/").getFile());
+            json = new JSONArray(data);
             return new ResponseEntity<String>(json.toString(), header, HttpStatus.OK);
-        } catch(IOException e) {
-            json.put("code", 404);
-            json.put("message", "Resource Not Found");
-            json.put("data", JSONObject.NULL);
-
-            return new ResponseEntity<String>(json.toString(), HttpStatus.NOT_FOUND);
+        } catch (IOException e) {
+            return new ResponseEntity<>(null, HttpStatus.NOT_FOUND);
         }
     }
 
@@ -54,17 +41,12 @@ public class PokemonController {
         
         try {
             String data = readFile(getClass().getResource("/pokemon/" + id + ".json").getFile());
-            json.put("code", 200);
-            json.put("message", "Success");
-            json.put("data", new JSONObject(data));
+            json = new JSONObject(data);
 
             return new ResponseEntity<String>(json.toString(), header, HttpStatus.OK);
         } catch (IOException e) {
-            json.put("code", 404);
-            json.put("message", "Resource Not Found");
-            json.put("data", JSONObject.NULL);
 
-            return new ResponseEntity<String>(json.toString(), header, HttpStatus.NOT_FOUND);
+            return new ResponseEntity<>(null, HttpStatus.NOT_FOUND);
         }
     }
 
