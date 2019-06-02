e6df25a9ca3e40aca92904f3a0ea17770b060151
nicolas <nicola010256@gmail.com>
2019-06-02 18:45:42 -0300

    ADD: PROCEDURE

--- src/main/java/pokemon/server/persistence/dao/TeamRepository.java
+++ src/main/java/pokemon/server/persistence/dao/TeamRepository.java
@@ -4,6 +4,7 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.jpa.repository.query.Procedure;
 import org.springframework.data.repository.query.Param;
 
 import pokemon.server.persistence.model.Team;
@@ -13,7 +14,9 @@ public interface TeamRepository extends JpaRepository<Team, Team.Id> {
     @Query("SELECT t FROM Team t where t.id.username = :username")
     Page<Team> findByUsername(@Param("username") String username, Pageable pageable);
 
-    @Query("SELECT (COUNT(t.id.teamId) + 1) FROM Team t")
+
+    // @Query("SELECT (COUNT(t.id.teamId) + 1) FROM Team t")
+    @Procedure(procedureName = "TEAM_FREE_ID")
     int nextFreeId();
 
 }
