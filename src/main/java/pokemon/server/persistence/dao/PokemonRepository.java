093f885858cef50b0c5c44fc5498bfb5d8d8f558
pastoreli <pastoreli.igor@yahoo.com>
2019-06-19 20:31:27 -0300

    fix

--- src/main/java/pokemon/server/persistence/dao/PokemonRepository.java
+++ src/main/java/pokemon/server/persistence/dao/PokemonRepository.java
@@ -1,15 +1,21 @@
 package pokemon.server.persistence.dao;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.query.Procedure;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
 import pokemon.server.persistence.model.Pokemon;
 
 public interface PokemonRepository extends JpaRepository<Pokemon, Pokemon.Id> {
 
-    // @Query("SELECT (COUNT(p.id.teamPokemonId) + 1) FROM Pokemon p where p.id.username = :username AND p.id.teamId = :teamId")
-    @Procedure(procedureName = "POKEMON_FREE_ID")
-    int nextFreeId(@Param("USERNAME_IN") String username, @Param("TEAM_ID_IN") int id);
+    // @Query("SELECT  case when (max(p.id.teamPokemonId) + 1) IS NULL THEN 1 ELSE max(p.id.teamPokemonId) + 1) END FROM Pokemon p where p.id.username = :username AND p.id.teamId = :teamId")
+    // @Procedure(procedureName = "POKEMON_FREE_ID")
+    @Query(
+        value = "SELECT case when MAX(ID) is NULL then 1 else MAX(ID) + 1 END FROM POKEMON WHERE USERNAME = :username AND TEAM_ID = :teamId",
+        nativeQuery = true
+    )
+    int nextFreeId(
+        @Param("username") String username, 
+        @Param("teamId") int id);
 
 }
--- src/main/java/pokemon/server/persistence/dao/TeamRepository.java
+++ src/main/java/pokemon/server/persistence/dao/TeamRepository.java
@@ -4,7 +4,6 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.jpa.repository.query.Procedure;
 import org.springframework.data.repository.query.Param;
 
 import pokemon.server.persistence.model.Team;
@@ -15,8 +14,12 @@ public interface TeamRepository extends JpaRepository<Team, Team.Id> {
     Page<Team> findByUsername(@Param("username") String username, Pageable pageable);
 
 
-    // @Query("SELECT (COUNT(t.id.teamId) + 1) FROM Team t")
-    @Procedure(procedureName = "TEAM_FREE_ID")
-    int nextFreeId(String username);
+    // @Query("SELECT CASE WHEN (MAX(t.id.teamId) + 1) IS NULL THEN 1 ELSE (MAX(t.id.teamId) + 1) END FROM Team t WHERE t.id.username = :username")
+    // @Procedure(procedureName = "TEAM_FREE_ID")
+    @Query(
+        value = "SELECT case when MAX(ID) is NULL THEN 1 else MAX(ID) + 1 END FROM TEAM WHERE USERNAME = :username",
+        nativeQuery = true
+    )
+    int nextFreeId(@Param("username") String username);
 
 }
